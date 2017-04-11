d6e75c835b0bf07524437d7196b26cac13b4f530
Cristian Pintea <cristian@pintea.net>
2017-04-11 23:11:04 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,185 @@
+'use strict';
+
+var ytdl = require('ytdl-core');
+const fs = require('fs');
+var libQ = require('kew');
+
+module.exports = PiYoutubeQueue;
+
+function PiYoutubeQueue(context) {
+	var self = this;
+
+  this.context = context;
+  this.commandRouter = this.context.coreCommand;
+  this.logger = this.context.logger;
+  this.configManager = this.context.onfigManager;
+}
+
+PiYoutubeQueue.prototype.onVolumioStart = function () {
+	var self = this;
+
+	return libQ.resolve();
+};
+
+PiYoutubeQueue.prototype.onStart = function(){
+  //Getting the mdp plugin
+  this.mpdPlugin = this.commandRouter.pluginManager.getPlugin('music_service', 'mpd');
+ 	return libQ.resolve();
+}
+
+PiYoutubeQueue.prototype.onStop = function () {
+	var self = this;
+var defer=libQ.defer();
+return defer.promise;
+	//Perform stop tasks here
+};
+
+PiYoutubeQueue.prototype.onRestart = function () {
+	var self = this;
+var defer=libQ.defer();
+return defer.promise;
+	//Perform restart tasks here
+};
+
+PiYoutubeQueue.prototype.onInstall = function () {
+	var self = this;
+	//Perform your installation tasks here
+};
+
+PiYoutubeQueue.prototype.onUninstall = function () {
+	var self = this;
+	//Perform your deinstallation tasks here
+};
+
+PiYoutubeQueue.prototype.getUIConfig = function () {
+	var self = this;
+
+	return {success: true, plugin: "example_plugin"};
+};
+
+PiYoutubeQueue.prototype.setUIConfig = function (data) {
+	var self = this;
+	//Perform your UI configuration tasks here
+};
+
+PiYoutubeQueue.prototype.getConf = function (varName) {
+	var self = this;
+	//Perform your tasks to fetch config data here
+};
+
+PiYoutubeQueue.prototype.setConf = function (varName, varValue) {
+	var self = this;
+	//Perform your tasks to set config data here
+};
+
+//Optional functions exposed for making development easier and more clear
+PiYoutubeQueue.prototype.getSystemConf = function (pluginName, varName) {
+	var self = this;
+	//Perform your tasks to fetch system config data here
+};
+
+PiYoutubeQueue.prototype.setSystemConf = function (pluginName, varName) {
+	var self = this;
+	//Perform your tasks to set system config data here
+};
+
+PiYoutubeQueue.prototype.getAdditionalConf = function () {
+	var self = this;
+	//Perform your tasks to fetch additional config data here
+};
+
+PiYoutubeQueue.prototype.setAdditionalConf = function () {
+	var self = this;
+	//Perform your tasks to set additional config data here
+};
+
+PiYoutubeQueue.prototype.play2 = function(uri){
+	var self = this;
+
+	ytdl.getInfo(uri, { filter: "audioonly"}, function(err, info){
+		if(err){
+			console.log("Error opening Youtube stream, video is probably not valid.");
+		}else {
+			self.commandRouter.addQueueItems({uri: uri, service: 'youtube', name: info.title, type: 'track'});
+		}
+		});
+
+}
+
+PiYoutubeQueue.prototype.pause = function(){
+	var self = this;
+	this.logger.info("PiYoutubeQueue::pause");
+
+	return self.mpdPlugin.sendMpdCommand('pause',[]);
+};
+
+
+PiYoutubeQueue.prototype.play = function(vuri){
+  var self = this;
+  this.logger.info("PiYoutubeQueue::play");
+	self.commandRouter.addQueueItems([{
+		uri: vuri,
+		service:"PiYoutubeQueue"
+	}]);
+};
+
+PiYoutubeQueue.prototype.stop = function(){
+	this.logger.info("PiYoutubeQueue::stop");
+
+
+	this.mpdPlugin.sendMpdCommand('stop',[]);
+	return libQ.resolve();
+}
+
+PiYoutubeQueue.prototype.explodeUri = function(uri) {
+	var self = this;
+
+	var defer=libQ.defer();
+
+	ytdl.getInfo(uri, { filter: "audioonly"}, function(err, info){
+		if(err){
+			console.log("Error opening Youtube stream, video is probably not valid.");
+		}else {
+
+			console.log(info.title)
+			defer.resolve({
+				uri: info["formats"][0]["url"],
+				service: 'PiYoutubeQueue',
+				name: info.title,
+				type: 'track',
+				albumart: info["thumbnail_url"]
+			});
+		}
+	})
+
+	return defer.promise;
+};
+
+PiYoutubeQueue.prototype.addToBrowseSources = function () {
+
+	var data = {name: 'Youtube', uri: 'PiYoutubeQueue', plugin_type:'music_service', plugin_name:'PiYoutubeQueue'};
+	return this.commandRouter.volumioAddToBrowseSources(data);
+};
+
+PiYoutubeQueue.prototype.clearAddPlayTrack = function(track){
+	console.log("PiYoutubeQueue::clearAddPlayTrack");
+	var self = this;
+
+	return self.mpdPlugin.sendMpdCommand('stop',[])
+        .then(function()
+        {
+            return self.mpdPlugin.sendMpdCommand('clear',[]);
+        })
+        .then(function()
+        {
+            return self.mpdPlugin.sendMpdCommand('load "'+track.uri+'"',[]);
+        })
+        .fail(function (e) {
+            return self.mpdPlugin.sendMpdCommand('add "'+track.uri+'"',[]);
+        })
+        .then(function()
+        {
+            self.commandRouter.stateMachine.setConsumeUpdateService('mpd');
+            return self.mpdPlugin.sendMpdCommand('play',[]);
+        });
+}
