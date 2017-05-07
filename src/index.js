b2077da1d17ce42ab71027c39dcfccc4d1c434f7
sla89 <skullsplitter@die-optimisten.net>
2017-05-07 15:07:53 +0200

    manual authentication

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 'use strict';
 
+var http = require('http');
+var spawn = require('child_process').spawn;
+var url = require('url');
+var querystring = require('querystring');
+var fs = require('fs');
 var ytdl = require('ytdl-core');
 var libQ = require('kew');
+var path = require('path');
 var gapis = require('googleapis');
 var request = require('request');
 var dur = require("iso8601-duration");
+var OAuth2Client = gapis.auth.OAuth2;
+var querystring = require('querystring');
+var https = require('https');
 
-var yt = gapis.youtube('v3');
+var secrets = require('./auth.json');
+var token = require('./authToken.json');
+
 var ytapi_key = "AIzaSyAl1Xq9DwdE_KD4AtPaE4EJl3WZe2zCqg4";
 
 module.exports = Youtube;
@@ -14,16 +25,21 @@ module.exports = Youtube;
 function Youtube(context) {
   var self = this;
 
-  this.context = context;
-  this.commandRouter = this.context.coreCommand;
-  this.logger = this.context.logger;
-  this.configManager = this.context.configManager;
-  this.addQueue = [];
-  this.trendResults = [];
-  this.searchResults = [];
-  this.playlistItems = [];
-  this.state = {};
-  this.stateMachine = this.commandRouter.stateMachine;
+  self.context = context;
+  self.commandRouter = self.context.coreCommand;
+  self.logger = self.context.logger;
+  self.configManager = self.context.configManager;
+  self.addQueue = [];
+  self.trendResults = [];
+  self.searchResults = [];
+  self.playlistItems = [];
+  self.state = {};
+  self.stateMachine = self.commandRouter.stateMachine;
+
+  self.yt = gapis.youtube({
+    version: 'v3',
+    auth: ytapi_key
+  });
 }
 
 Youtube.prototype.onVolumioStart = function() {
@@ -36,14 +52,23 @@ Youtube.prototype.onVolumioStart = function() {
   self.config.loadFile(configFile);
   self.resultsLimit = self.config.get('results_limit');
 
+  self.getAccessToken();
+
+  if (self.isAccessGranted()) {
+    self.accessToken = token;
+    self.refreshAuthToken();
+    self.updateYtApiAccessToken();
+  }
+
   return libQ.resolve();
 };
 
 Youtube.prototype.onStart = function() {
+  var self = this;
   //Getting the mdp plugin
-  this.mpdPlugin = this.commandRouter.pluginManager.getPlugin('music_service', 'mpd');
-  this.logger.info("Youtube::onStart Adding to browse sources");
-  this.addToBrowseSources();
+  self.mpdPlugin = self.commandRouter.pluginManager.getPlugin('music_service', 'mpd');
+  self.logger.info("Youtube::onStart Adding to browse sources");
+  self.addToBrowseSources();
   return libQ.resolve();
 }
 
@@ -69,14 +94,20 @@ Youtube.prototype.onUninstall = function() {
 
 Youtube.prototype.getUIConfig = function() {
   var self = this;
-  var lang_code = this.commandRouter.sharedVars.get('language_code');
-
-  return self.commandRouter.i18nJson(__dirname + '/i18n/strings_' + lang_code + '.json',
+  var lang_code = self.commandRouter.sharedVars.get('language_code');
+  return self.commandRouter.i18nJson(path.join(__dirname, 'i18n', 'strings_' + lang_code + '.json'),
       __dirname + '/i18n/strings_en.json',
       __dirname + '/UIConfig.json')
     .then(function(uiconf) {
-      uiconf.sections[0].content[0].value = self.config.get('results_limit');
+      if (self.accessToken != null) {
+        uiconf.sections[0].description = 'Volumio has access to your YouTube account. We will only use it to display videos related to your account!';
+      } else {
+        self.pollPermissions();
+        uiconf.sections[0].description = uiconf.sections[0].description.replace('{0}', self.accessData.verification_url);
+        uiconf.sections[0].content[0].value = self.accessData.user_code;
+      }
 
+      uiconf.sections[1].content[0].value = self.config.get('results_limit');
       return uiconf;
     })
     .fail(function() {
@@ -105,7 +136,7 @@ Youtube.prototype.getConfigurationFiles = function() {
 
 Youtube.prototype.pause = function() {
   var self = this;
-  this.logger.info("Youtube::pause");
+  self.logger.info("Youtube::pause");
 
   return self.commandRouter.volumioPause();
 };
@@ -113,24 +144,24 @@ Youtube.prototype.pause = function() {
 
 Youtube.prototype.add = function(vuri) {
   var self = this;
-  this.logger.info("Youtube::add");
+  self.logger.info("Youtube::add");
 
   if (vuri.includes("list=")) {
     var playlistId = decodeURI(vuri.toString()).split("list=")[1].split("&")[0];
-    this.addPlaylist(playlistId);
+    self.addPlaylist(playlistId);
   } else {
     var regex = /https?:\/\/(?:[0-9A-Z-]+\.)?(?:youtu\.be\/|youtube(?:-nocookie)?\.com\S*?[^\w\s-])([\w-]{11})(?=[^\w-]|$)(?![?=&+%\w.-]*(?:['"][^<>]*>|<\/a>))[?=&+%\w.-]*/ig;
     var videoId = vuri.replace(regex, "$1");
     self.commandRouter.pushConsoleMessage(vuri + " kkk " + videoId);
-    this.addVideo(videoId);
+    self.addVideo(videoId);
   }
 };
 
 Youtube.prototype.addVideo = function(videoId) {
   var self = this;
-  this.logger.info("Youtube::addVideo");
+  self.logger.info("Youtube::addVideo");
 
-  return this.commandRouter.addQueueItems([{
+  return self.commandRouter.addQueueItems([{
     uri: videoId,
     service: "youtube"
   }]);
@@ -138,11 +169,10 @@ Youtube.prototype.addVideo = function(videoId) {
 
 Youtube.prototype.addPlaylist = function(playlistId, pageToken) {
   var self = this;
-  this.logger.info("Youtube::addPlaylist " + playlistId);
+  self.logger.info("Youtube::addPlaylist " + playlistId);
 
   //Contact Youtube Data API v3 for the list of videos in the playlist
   var request = {
-    auth: ytapi_key,
     part: "snippet",
     maxResults: 50,
     playlistId: playlistId
@@ -151,7 +181,7 @@ Youtube.prototype.addPlaylist = function(playlistId, pageToken) {
   if (pageToken != undefined)
     request.pageToken = pageToken;
 
-  yt.playlistItems.list(request, function(err, res) {
+  self.yt.playlistItems.list(request, function(err, res) {
     if (err) {
       //Holy crap, something went wrong :/
       self.logger.error(err.message + "\n" + err.stack);
@@ -203,9 +233,9 @@ Youtube.prototype.parseAddQueue = function() {
 }
 
 Youtube.prototype.stop = function() {
-  this.logger.info("Youtube::stop");
-
-  return this.commandRouter.volumioStop();
+  var self = this;
+  self.logger.info("Youtube::stop");
+  return self.commandRouter.volumioStop();
 }
 
 Youtube.prototype.handleBrowseUri = function(uri) {
@@ -215,6 +245,10 @@ Youtube.prototype.handleBrowseUri = function(uri) {
   if (uri.startsWith('youtube')) {
     //root
     if (uri === 'youtube') {
+      if (self.isAccessGranted()) {
+        return self.getActivities();
+      }
+
       return self.getTrend();
     } else if (uri.startsWith('youtube/playlist/')) {
       return self.getPlaylistItems(uri.split('/').pop());
@@ -226,7 +260,6 @@ Youtube.prototype.handleBrowseUri = function(uri) {
 
 Youtube.prototype.explodeUri = function(uri) {
   var self = this;
-
   var deferred = libQ.defer();
 
   if (uri.startsWith('youtube/playlist/')) {
@@ -236,8 +269,8 @@ Youtube.prototype.explodeUri = function(uri) {
   } else {
     self.logger.info("Youtube::explodeUri " + "https://youtube.com/oembed?format=json&url=" + uri);
 
-    yt.videos.list({
-      auth: ytapi_key,
+
+    self.yt.videos.list({
       part: "snippet,contentDetails",
       id: uri
     }, function(err, res) {
@@ -279,18 +312,18 @@ Youtube.prototype.search = function(query) {
 };
 
 Youtube.prototype.getState = function() {
-  this.logger.info("Youtube::getState");
+  var self = this;
+  self.logger.info("Youtube::getState");
 };
 
 Youtube.prototype.addToBrowseSources = function() {
-  var data = {
+  var self = this;
+  self.commandRouter.volumioAddToBrowseSources({
     name: 'Youtube',
     uri: 'youtube',
     plugin_type: 'music_service',
     plugin_name: 'youtube'
-  };
-
-  this.commandRouter.volumioAddToBrowseSources(data);
+  });
 };
 
 Youtube.prototype.clearAddPlayTrack = function(track) {
@@ -384,7 +417,7 @@ Youtube.prototype.resume = function() {
 
 Youtube.prototype.seek = function(position) {
   var self = this;
-  this.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'Youtube::seek');
+  self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'Youtube::seek');
 
   //self.commandRouter.stateMachine.setConsumeUpdateService('youtube', true);
   return self.mpdPlugin.seek(position);
@@ -392,8 +425,8 @@ Youtube.prototype.seek = function(position) {
 
 Youtube.prototype.next = function() {
   var self = this;
-  this.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'Youtube::next');
-  return this.mpdPlugin.sendMpdCommand('next', []).then(function() {
+  self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'Youtube::next');
+  return self.mpdPlugin.sendMpdCommand('next', []).then(function() {
     return self.mpdPlugin.getState().then(function(state) {
       state.trackType = "Fucking Youtube!!!!";
       return self.commandRouter.stateMachine.syncState(state, "youtube");
@@ -402,8 +435,9 @@ Youtube.prototype.next = function() {
 };
 
 Youtube.prototype.previous = function() {
-  this.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'Youtube::previous');
-  return this.mpdPlugin.sendMpdCommand('previous', []).then(function() {
+  var self = this;
+  self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'Youtube::previous');
+  return self.mpdPlugin.sendMpdCommand('previous', []).then(function() {
     return self.mpdPlugin.getState().then(function(state) {
       state.trackType = "Fucking Youtube!!!!";
       return self.commandRouter.stateMachine.syncState(state, "youtube");
@@ -413,7 +447,7 @@ Youtube.prototype.previous = function() {
 
 Youtube.prototype.prefetch = function(nextTrack) {
   var self = this;
-  this.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'Youtube::prefetch');
+  self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'Youtube::prefetch');
 
   ytdl.getInfo("https://youtube.com/watch?v=" + nextTrack.uri, {
     filter: "audioonly"
@@ -437,7 +471,6 @@ Youtube.prototype.getTrend = function(pageToken, deferred) {
   }
 
   var request = {
-    auth: ytapi_key,
     chart: 'mostPopular',
     part: "snippet",
     maxResults: 50
@@ -447,7 +480,7 @@ Youtube.prototype.getTrend = function(pageToken, deferred) {
     request.pageToken = pageToken;
   }
 
-  yt.videos.list(request, function(err, res) {
+  self.yt.videos.list(request, function(err, res) {
     if (err) {
       self.logger.error(err.message + "\n" + err.stack);
       deferred.reject(err);
@@ -492,7 +525,6 @@ Youtube.prototype.doSearch = function(query, pageToken, deferred) {
   }
 
   var request = {
-    auth: ytapi_key,
     q: query,
     part: "snippet",
     type: "playlist, video",
@@ -503,7 +535,7 @@ Youtube.prototype.doSearch = function(query, pageToken, deferred) {
     request.pageToken = pageToken;
   }
 
-  yt.search.list(request, function(err, res) {
+  self.yt.search.list(request, function(err, res) {
     if (err) {
       self.logger.error(err.message + "\n" + err.stack);
       deferred.reject(err);
@@ -541,7 +573,6 @@ Youtube.prototype.getPlaylistItems = function(playlistId, pageToken, deferred) {
   }
 
   var request = {
-    auth: ytapi_key,
     playlistId: playlistId,
     part: "snippet",
     maxResults: 50
@@ -551,7 +582,7 @@ Youtube.prototype.getPlaylistItems = function(playlistId, pageToken, deferred) {
     request.pageToken = pageToken;
   }
 
-  yt.playlistItems.list(request, function(err, res) {
+  self.yt.playlistItems.list(request, function(err, res) {
     if (err) {
       self.logger.error(err.message + "\n" + err.stack);
       deferred.reject(err);
@@ -691,3 +722,200 @@ Youtube.prototype.updateSettings = function(data) {
 
   return libQ.resolve();
 };
+
+Youtube.prototype.loadI18NStrings = function(language_code) {
+  console.log(language_code);
+}
+
+Youtube.prototype.getAccessToken = function() {
+  var self = this;
+  var deferred = libQ.defer();
+
+  var postData = querystring.stringify({
+    'client_id': secrets.volumio.client_id,
+    'scope': 'https://www.googleapis.com/auth/youtube.readonly'
+  });
+
+  var options = {
+    host: 'accounts.google.com',
+    path: '/o/oauth2/device/code',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(postData)
+    }
+  };
+
+  // Set up the request
+  var codeReq = https.request(options, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function(chunk) {
+      self.accessData = JSON.parse(chunk);
+      deferred.resolve(self.accessData);
+    });
+  });
+
+  codeReq.on('error', (e) => {
+    deferred.reject(e);
+  });
+
+  codeReq.write(postData);
+  codeReq.end();
+
+  return deferred.promise;
+}
+
+Youtube.prototype.pollPermissions = function() {
+  var self = this;
+  var deferred = libQ.defer();
+
+  var postData = querystring.stringify({
+    'client_id': secrets.volumio.client_id,
+    'client_secret': secrets.volumio.client_secret,
+    'code': self.accessData.device_code,
+    'grant_type': 'http://oauth.net/grant_type/device/1.0'
+  });
+
+  var options = {
+    host: 'www.googleapis.com',
+    path: '/oauth2/v4/token',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(postData)
+    }
+  };
+
+  // Set up the request
+  var codeReq = https.request(options, function(res) {
+    res.setEncoding('utf8');
+
+    res.on('data', function(chunk) {
+      var data = JSON.parse(chunk);
+      console.log(res.statusCode, data);
+
+      if (res.statusCode == '200') {
+        fs.writeFile(path.join(__dirname, 'authToken.json'), chunk, function(err) {
+          if (err) {
+            return console.log(err);
+          }
+          console.log("The file was saved!");
+        });
+
+        self.accessToken = data;
+        self.commandRouter.pushToastMessage('success', 'Access granted', 'Volumio has now access to your YouTube account.');
+        self.updateYtApiAccessToken();
+
+        deferred.resolve(data);
+      } else if (res.statusCode == '400') {
+        //Authorization pending
+        console.log('Authorization pending, polling again in ' + self.accessData.interval + ' seconds.');
+        setTimeout(self.pollPermissions.bind(self), self.accessData.interval * 1000);
+      } else {
+        self.commandRouter.pushToastMessage('error', data.error, data.error_description);
+        deferred.reject(new Error(data.error + ': ' + data.error_description));
+      }
+    });
+  });
+
+  codeReq.on('error', (e) => {
+    deferred.reject(e);
+  });
+
+  codeReq.write(postData);
+  codeReq.end();
+
+  return deferred.promise;
+}
+
+Youtube.prototype.refreshAuthToken = function() {
+  var self = this;
+  var deferred = libQ.defer();
+
+  var postData = querystring.stringify({
+    'client_id': secrets.volumio.client_id,
+    'client_secret': secrets.volumio.client_secret,
+    'refresh_token': self.accessToken.refresh_token,
+    'grant_type': 'refresh_token'
+  });
+
+  var options = {
+    host: 'www.googleapis.com',
+    path: '/oauth2/v4/token',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(postData)
+    }
+  };
+
+  // Set up the request
+  var codeReq = https.request(options, function(res) {
+    res.setEncoding('utf8');
+
+    res.on('data', function(chunk) {
+      var data = JSON.parse(chunk);
+      console.log(data);
+
+      if (res.statusCode == '200') {
+        self.accessToken.expires_in = data.expires_in;
+        self.accessToken.access_token = data.access_token;
+
+        fs.writeFile(path.join(__dirname, 'authToken.json'), JSON.stringify(self.accessToken), function(err) {
+          if (err) {
+            return console.log(err);
+          }
+          console.log("The file was saved!");
+        });
+
+        setTimeout(self.refreshAuthToken.bind(self), self.accessToken.expires_in * 1000);
+        self.updateYtApiAccessToken();
+
+        console.log('refresh token again in seconds: ' + self.accessToken.expires_in);
+
+        deferred.resolve({});
+      } else {
+        console.log('----------------------------> CLEARING FILE');
+
+        self.commandRouter.pushToastMessage('error', data.error, data.error_description);
+        fs.writeFile(path.join(__dirname, 'authToken.json'), JSON.stringify({}), function(err) {
+          if (err) {
+            return console.log(err);
+          }
+          console.log("The file was saved!");
+        });
+        deferred.reject(new Error(data.error + ': ' + data.error_description));
+      }
+    });
+  });
+
+  codeReq.on('error', (e) => {
+    deferred.reject(e);
+  });
+
+  codeReq.write(postData);
+  codeReq.end();
+
+  return deferred.promise;
+}
+
+Youtube.prototype.updateYtApiAccessToken = function() {
+  var self = this;
+
+  var oauth2Client = new OAuth2Client(
+    secrets.volumio.client_id,
+    secrets.volumio.client_secret,
+    secrets.volumio.redirect_uris[0]
+  );
+
+  oauth2Client.setCredentials(self.accessToken);
+
+  self.yt = gapis.youtube({
+    version: 'v3',
+    auth: oauth2Client
+  });
+}
+
+Youtube.prototype.isAccessGranted = function() {
+  return token != null && token.refresh_token != null && token.access_token != null;
+}
